1c1941ed7d73e4327069a9b44fb2fd8f9b9e6aef
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-03-13 14:08:47 +0100

    Initial commit

---
+++ Assets/Sangre.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Sangre : MonoBehaviour
+{
+    PostProcessVolume ppv;//Componente
+    Vignette vignetteLayer;//Seccion
+    Grain grainLayer;
+    private void Start()
+    {
+        ppv = GetComponent<PostProcessVolume>();//Obtenemos el componente
+        ppv.profile.TryGetSettings(out vignetteLayer);//Cargamos la referencia a la seccion
+        ppv.profile.TryGetSettings(out grainLayer);
+    }
+    void Update()
+    {
+        vignetteLayer.opacity.value += Time.deltaTime;
+        if (vignetteLayer.opacity.value >= 1)
+        {
+            grainLayer.active = true;
+        }
+    }
+}
